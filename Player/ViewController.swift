3a90d5b6e75e876c9cea065fcb18a794da5d1cb1
noamohana <nohana@lightricks.com>
2021-06-18 15:38:59 +0300

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by Noam Ohana on 18/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
